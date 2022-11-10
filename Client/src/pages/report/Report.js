704280b8edac694cc9f73c3c6db43a8a50980f62
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-11-10 21:31:45 +0600

    Initiated  report page

--- Client/src/pages/report/Report.js
+++ Client/src/pages/report/Report.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import StockReportTable from '../../components/table/StockReportTable'
+import "./Report.css"
 
 export const Report = () => {
   return (
-    <div>Report</div>
+    <div className='reportMain'>
+      <h2>Stock Report</h2>
+      <StockReportTable/>
+    </div>
   )
 }

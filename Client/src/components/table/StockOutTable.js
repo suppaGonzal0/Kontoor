58a8d4679351a51973d5a3294ec62b98335ef667
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-11-10 21:30:36 +0600

    Tables
    Responsive layout

---
+++ Client/src/components/table/StockOutTable.js
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+
+const styles = {
+  fontSize: "0.87rem",
+  cursor: "pointer",
+  color: "#2B67B2"
+}
+
+const columns = [
+  { id: 'remove', label: '', minWidth: 30, align: 'center'},
+  { id: 'batchId', label: 'Batch\u00a0ID', minWidth: 60, align: 'center'},
+  { id: 'date', label: 'Date', minWidth: 75, align: 'center'},
+  { id: 'quantity', label: 'Quantity', minWidth: 60, align: 'center'},
+  { id: 'by', label: 'By', minWidth: 90, align: 'center'}
+];
+
+function createData(remove, batchId, date, quantity, by) {
+  return {remove, batchId, date, quantity, by};
+}
+
+const data = [
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"},
+  {batchId: '3287263', date: '6-Jun-2022', quantity: 1324171354, by: "Afrina Akhter"}
+];
+
+let rows = [];
+  data.map(mp => rows.push(createData(<RiDeleteBin5Fill style={styles} />, mp.batchId, mp.date, mp.quantity, mp.by)))
+
+export default function StockOutTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden', }}>
+      <TableContainer sx={{ maxHeight: 590}}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
---
+++ Client/src/components/table/StockReportTable.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 100 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 100,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 100,
+    align: 'right',
+    format: (value) => value.toLocaleString('en-US'),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 100,
+    align: 'right',
+    format: (value) => value.toFixed(2),
+  },
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const data = [
+  {name: 'India', code: 'IN', population: 1324171354, size: 3287263},
+  {name: 'China', code: 'CN', population: 1403500365, size: 1403500365},
+  {name: 'Italy', code: 'IT', population: 60483973, size: 301340},
+  {name: 'Canada', code: 'CA', population: 37602103, size: 9984670},
+  {name: 'Mexico', code: 'MX', population: 126577691, size: 1972550},
+  {name: 'India', code: 'IN', population: 1324171354, size: 3287263},
+  {name: 'India', code: 'IN', population: 1324171354, size: 3287263},
+];
+
+let rows = [];
+  data.map(mp => rows.push(createData(mp.name, mp.code, mp.population, mp.size)))
+
+export default function StockReportTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 600 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
